f36243ed01d3e2e699e369b4fe94abc7d0685435
Gurpreet Bhoot <gur_boo15@Gurpreets-MacBook-Pro.local>
2018-07-07 15:20:49 -0700

    added create account VC with outlets and actions

--- smack-chat/smack-chat/Controller/LoginVC.swift
+++ smack-chat/smack-chat/Controller/LoginVC.swift
@@ -24,4 +24,9 @@ class LoginVC: UIViewController {
     @IBAction func closeBtnPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+    
+    @IBAction func createAccountPressed(_ sender: Any) {
+        performSegue(withIdentifier: TO_CREATE, sender: nil)
+    }
+    
 }
