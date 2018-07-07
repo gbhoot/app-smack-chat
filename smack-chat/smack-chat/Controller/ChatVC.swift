948f7595befecaec2738c5f74cb89dcb7b7dd856
Gurpreet Bhoot <gur_boo15@Gurpreets-MacBook-Pro.local>
2018-07-07 12:20:18 -0700

    added sw-reveal views for sliding side menu

---
+++ smack-chat/smack-chat/Controller/ChatVC.swift
@@ -0,0 +1,31 @@
+//
+//  ChatVC.swift
+//  smack-chat
+//
+//  Created by Gurpreet Bhoot on 7/7/18.
+//  Copyright © 2018 Gurpal Bhoot. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    
+    // Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
